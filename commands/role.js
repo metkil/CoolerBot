0a8c1a5272c8aacb90ed02a2a032ed66a20c875b
Gabriel Kovalechyn <gkovalechyn@gmail.com>
2018-02-20 16:59:29 +0400

    Treated empty strings and no arguments

--- commands/role.js
+++ commands/role.js
@@ -8,17 +8,21 @@ module.exports = {
         var entireArgs = args.join(" ");
         var roles = entireArgs.split(",");
 
-        if (roles.length == 1){
-            message.channel.send(`${message.author} You didn't specify any roles.`);
-            return;
-        }
-
         //Get rid of all the empty spaces if there are any
         roles.map(function(role){
             role.trim()
         });
+        roles = roles.filter(function(role){
+            return role; //Filter out empty strings
+        });
+        //I have no idea why chaining map and filter didn't work even though it should
 
-        //message.channel.send(roles);
+        if (roles.length == 0){
+            message.channel.send(`${message.author} You didn't specify any roles.`);
+            return;
+        }
+
+        message.react("👍");
         //@TODO Put the roles in the google docs sheet.
         //Just remember to limit it to 3 roles
     }

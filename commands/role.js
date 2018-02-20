05333c8b2c35d40165e740a2bdb48da96b29b378
Gabriel Kovalechyn <gkovalechyn@gmail.com>
2018-02-20 18:30:52 +0400

    Added a queue to update the sheet all at once

--- commands/role.js
+++ commands/role.js
@@ -1,9 +1,9 @@
-module.exports = {
+var self = module.exports = {
     name: "role",
     description: "",
     aliases: [],
     usage: "<role1> [role2] [role3]",
-    cooldown: 3,
+    cooldown: 0,
     execute(message, args){
         var roles = args.join(" ").split(",");
 
@@ -20,7 +20,43 @@ module.exports = {
         }
 
         message.react("👍");
+
+        self.queueSheetUpdate({
+            username: message.author.username,
+            roles: roles.slice(0, Math.min(3, roles.length))
+        });
+    },
+
+
+    queuedObjects: [],
+    lastTry: 0,
+    interval: 5000,
+    timeoutId: null,
+
+    queueSheetUpdate(obj){
+        module.exports.queuedObjects.push(obj);
+        module.exports.tryUpdate();
+    },
+
+    tryUpdate(){
+        var now = Date.now();
+
+        if (now - self.lastTry > self.interval){
+            self.lastTry = now;
+            self.updateSheet(self.queuedObjects);
+            self.queuedObjects = [];
+        }else{
+            if (module.exports.timeOutId){
+                clearTimeout(self.timeOutId);
+            }
+            self.timeOutId = setTimeout(self.tryUpdate, self.interval);
+            self.lastTry = now;
+        }
+    },
+
+    updateSheet(objects){
         //@TODO Put the roles in the google docs sheet.
-        //Just remember to limit it to 3 roles
     }
 }
+
+

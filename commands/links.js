01862d71d892d21164608a8a42d049f62b281699
Zacharie Small <Metkil5685@gmail.com>
2018-07-01 09:57:20 -0400

    Quick small updates
    Just getting files setup for editting.

---
+++ commands/links.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'links',
+	description: 'Links to various TCS related websites.',
+	aliases: ['link', 'amazon', 'donate', 'steam'],
+	guildonly: false,
+	execute(message) {
+		message.channel.send(`Hello there ${message.author}!`);
+	},
+
+	// Code goes here.  Want to use what ever the good looking text fields is called.
+};
--- commands/silence.js
+++ commands/silence.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: 'hello',
 	description: 'Hello!',
 	execute(message) {
-		message.channel.send(`Hello there ${message.author}!`);
+		message.channel.send('YOU CAN\'T SILENCE ME!!');
 	},
+
+	// Code to silence goes here.  Discord permissions instead of SQLLite perhaps?
 };
